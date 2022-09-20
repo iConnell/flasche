884d10c0362b1101e6471989be32f0416491d57b
McConnell <ik.ugwuanyi@gmail.com>
2022-09-20 13:26:06 +0100

    database setup

--- main.py
+++ main.py
@@ -9,5 +9,27 @@ db = SQLAlchemy(app)
 api = Api(app)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first_name = db.Column(db.String, nullable=False)
+    last_name = db.Column(db.String, nullable=False)
+    username = db.Column(db.String, nullable=False, unique=True)
+    email = db.Column(db.String, nullable=False, unique=True)
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name}"
+
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    title = db.Column(db.String, nullable=False)
+    likes = db.Column(db.Integer, nullable=False, default=0)
+    views = db.Column(db.Integer, nullable=False, default=0)
+
+    def __repr__(self):
+        return self.title
+
+
 if __name__ == '__main__':
     app.run(debug=True)
